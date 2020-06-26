dd738fcd5a5fa8d81312c77ad7d0ba3bcb94214e
Amruta kapse <kapseamruta50@gmail.com>
2020-06-26 16:56:11 +0530

    changes by Asian guy in develop branch

--- src/New.java
+++ src/New.java
@@ -4,7 +4,8 @@ public class New {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-
+		System.out.println("Friends");
+		System.out.println("I");
 	}
 
 }
